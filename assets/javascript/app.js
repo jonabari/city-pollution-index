b875d11f7a98bbb2b3c065a41ef95ac912f55e9d
gnefkow <gnefkow87@gmail.com>
2018-11-18 03:23:00 +0800

    add style to background, chinese characters for severity levels, etc...

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -9,6 +9,8 @@ var city
 var AQI
 var haze
 var fog
+let locationName;
+
 
 // airNow API call: https://docs.airnowapi.org/CurrentObservationsByLatLon/query
 function getPollutionIndex() {
@@ -39,5 +41,72 @@ $('#select-mexico').on('click', function () {
     console.log('===> I hear ya!')
 })
 
+function airQualityIndex() {
+  if (AQI >= 300){
+    document.getElementById("airQualityIndexName").className = "severe";
+    document.getElementById("airQualityIndexName").innerHTML = "严重污染";
+    document.getElementById("airQualityIndexNumber").className = "severe";
+    document.getElementById("airQualityIndexNumber").innerHTML = "六";
+    document.getElementById("locationName").className = "severe";
+  } else if (AQI <= 299 && AQI > 201 ){
+    document.getElementById("airQualityIndexName").className = "heavy";
+    document.getElementById("airQualityIndexName").innerHTML = "重度污染";
+    document.getElementById("airQualityIndexNumber").className = "heavy";
+    document.getElementById("airQualityIndexNumber").innerHTML = "五";
+    document.getElementById("locationName").className = "heavy";
+  } else if (AQI <= 200 && AQI > 151 ){
+    document.getElementById("airQualityIndexName").className = "moderate";
+    document.getElementById("airQualityIndexName").innerHTML = "中度污染";
+    document.getElementById("airQualityIndexNumber").className = "moderate";
+    document.getElementById("airQualityIndexNumber").innerHTML = "四";
+    document.getElementById("locationName").className = "moderate";
+  } else if (AQI <= 150 && AQI > 101 ){
+    document.getElementById("airQualityIndexName").className = "light";
+    document.getElementById("airQualityIndexName").innerHTML = "轻度污染";
+    document.getElementById("airQualityIndexNumber").className = "light";
+    document.getElementById("airQualityIndexNumber").innerHTML = "三";
+    document.getElementById("locationName").className = "light";
+  } else if (AQI <= 100 && AQI > 51 ){
+    document.getElementById("airQualityIndexName").className = "good";
+    document.getElementById("airQualityIndexName").innerHTML = "良";
+    document.getElementById("airQualityIndexNumber").className = "good";
+    document.getElementById("airQualityIndexNumber").innerHTML = "二";
+    document.getElementById("locationName").className = "good";
+  } else {
+    document.getElementById("airQualityIndexName").className = "excellent";
+    document.getElementById("airQualityIndexName").innerHTML = "优";
+    document.getElementById("airQualityIndexNumber").className = "excellent";
+    document.getElementById("airQualityIndexNumber").innerHTML = "一";
+    document.getElementById("locationName").className = "excellent";
+  }
+}
+
+function translateCity () {
+  locationName = city;
+    switch (locationName) {
+      case mexicoCity:
+      document.querySelector("#locationName").innerHTML = '墨西哥';
+      break;
+
+      case newYork:
+      document.querySelector("#locationName").innerHTML = '纽约';
+      break;
+
+      case dallas:
+      document.querySelector("#locationName").innerHTML = '达拉斯';
+      break;
+
+      case yangon:
+      document.querySelector("#locationName").innerHTML = '仰光';
+      break;
+
+      case denver:
+      document.querySelector("#locationName").innerHTML = '丹佛';
+      break;
+    }
+}
+
 // On Load
 getPollutionIndex()
+airQualityIndex()
+translateCity()
